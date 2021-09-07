3725e65afb3acc1ca7802c1a1004e425c0b428af
antonioal97 <antonioal97@gmail.com>
2021-09-07 20:43:14 -0300

    Modificacoes de estilo feitas de acordo com o projeto no Figma

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-let menuItemProtoArray = document.querySelectorAll('.menu__link')
-let menuItem = [...menuItemProtoArray];
-
-for(let item of menuItem){
-    item.addEventListener("mouseover", ()=>{
-        item.classList.contains('is-active') ? item.classList.remove('is-active') : item.classList.add('is-active'); 
-    })
-}
