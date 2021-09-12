1780d15e215f5693175a58d0d93760f5b7a3e979
antonioal97 <antonioal97@gmail.com>
2021-09-12 02:08:57 -0300

    Interaçao dos botoes com as cores dos cards

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,31 @@
-const card = document.querySelector('.c-card');
+const cardsLike = document.querySelectorAll('.c-card')
+const Cards = [...cardsLike]
+const card = document.querySelector('.c-card')
 
-const colors = {
-    primary: {
-        background: 'var(--dark-00)',
-        color 
-    }
-}
+const buttonsLike = document.querySelectorAll('.c-button')
+const Buttons = [...buttonsLike]
+const button = document.querySelector('.c-button')
+
+
+Cards.forEach(function(el){
+    el.addEventListener("mouseenter", function(){
+        if(el.classList.contains('clr-dev--card')){
+            Buttons.forEach(function(allButtons){
+                allButtons.classList.add('clr-dev')
+                allButtons.classList.remove('clr-DMM')
+            })
+        }else if(el.classList.contains('clr-DMM--card')){
+            Buttons.forEach(function(allButtons){
+                allButtons.classList.add('clr-DMM')
+                allButtons.classList.remove('clr-dev')
+            })
+        }else{
+            Buttons.forEach(function(allButtons){
+                allButtons.classList.remove('clr-dev')
+                allButtons.classList.remove('clr-DMM')
+                allButtons.classList.add('clr-primary')
+            })
+        }
+
+    })
+})
