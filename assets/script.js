254655fd1412076f964881c4bf63d146633a5f07
antonioal97 <antonioal97@gmail.com>
2021-09-12 18:56:16 -0300

    Modificacoes no script js

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,77 @@
-const cardsLike = document.querySelectorAll('.c-card')
-const Cards = [...cardsLike]
-const card = document.querySelector('.c-card')
+const DOM = {
+    cardsLike: document.querySelectorAll('.c-card'),
+    buttonsLike: document.querySelectorAll('.c-button'),
+    titlesLike: document.querySelectorAll('.c-card .typ-subtitle')
+}
 
-const buttonsLike = document.querySelectorAll('.c-button')
-const Buttons = [...buttonsLike]
-const button = document.querySelector('.c-button')
+const Card = {
+    Cards: [...DOM.cardsLike],
+    Buttons: [...DOM.buttonsLike],
+    Titles: [...DOM.titlesLike],
 
-
-Cards.forEach(function(el){
-    el.addEventListener("mouseenter", function(){
-        if(el.classList.contains('clr-dev--card')){
-            Buttons.forEach(function(allButtons){
-                allButtons.classList.add('clr-dev')
-                allButtons.classList.remove('clr-DMM')
+    changeTheme(){
+        this.Cards.forEach(function(el, index){
+            
+            
+            el.addEventListener("mouseenter", function(){
+                if(el.classList.contains('clr-dev--card')){
+                    
+                    Card.Buttons.forEach(function(allButtons, index){
+                        allButtons.classList.remove('clr-DMM')
+                        allButtons.classList.add('clr-dev')
+                    })
+                }else if(el.classList.contains('clr-DMM--card')){
+                    Card.Buttons.forEach(function(allButtons){
+                        allButtons.classList.remove('clr-dev')
+                        allButtons.classList.add('clr-DMM')
+                    })
+                }else{
+                    Card.Buttons.forEach(function(allButtons){
+                        allButtons.classList.remove('clr-dev')
+                        allButtons.classList.remove('clr-DMM')
+                        allButtons.classList.add('clr-primary')
+                    })
+                }
             })
-        }else if(el.classList.contains('clr-DMM--card')){
-            Buttons.forEach(function(allButtons){
-                allButtons.classList.add('clr-DMM')
-                allButtons.classList.remove('clr-dev')
-            })
-        }else{
-            Buttons.forEach(function(allButtons){
-                allButtons.classList.remove('clr-dev')
-                allButtons.classList.remove('clr-DMM')
-                allButtons.classList.add('clr-primary')
-            })
-        }
+        })
+    }
+}
 
-    })
-})
+Card.changeTheme()
+
+// const cardsLike = document.querySelectorAll('.c-card')
+// const Cards = [...cardsLike]
+
+// const buttonsLike = document.querySelectorAll('.c-button')
+// const Buttons = [...buttonsLike]
+
+// const titleLike = document.querySelectorAll('.c-card .typ-subtitle');
+// const cardsTitle = [...titleLike]
+
+
+// Cards.forEach(function(el){
+//     el.addEventListener("mouseenter", function(){
+        
+//         h2Before.classList.add('typ-subtitle--after')
+//         h2Before.style.color = "red"
+
+
+//         if(el.classList.contains('clr-dev--card')){
+//             Buttons.forEach(function(allButtons){
+//                 allButtons.classList.add('clr-dev')
+//                 allButtons.classList.remove('clr-DMM')
+//             })
+//         }else if(el.classList.contains('clr-DMM--card')){
+//             Buttons.forEach(function(allButtons){
+//                 allButtons.classList.add('clr-DMM')
+//                 allButtons.classList.remove('clr-dev')
+//             })
+//         }else{
+//             Buttons.forEach(function(allButtons){
+//                 allButtons.classList.remove('clr-dev')
+//                 allButtons.classList.remove('clr-DMM')
+//                 allButtons.classList.add('clr-primary')
+//             })
+//         }
+//     })
+// })
